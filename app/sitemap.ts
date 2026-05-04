0a91ed94d35f8ac660dba3b11227147c1fc1a7b3
Marcus Koch <kochmarcus00@gmail.com>
2026-05-04 19:44:19 +0200

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.ich-habs-im-griff.de",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
