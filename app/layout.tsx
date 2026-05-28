7ff9f0ceee7fd2bfb29bc03e9ceb939e0f1dbf12
Marcus Koch <kochmarcus00@gmail.com>
2026-05-28 16:09:43 +0200

    added umami analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="f9ddda35-2fd4-45e0-81b5-2c5eb0a9b1a8"
+        />
+      </body>
     </html>
   );
 }
