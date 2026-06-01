f5b07737d14c3c640c3ed9713d8d6a53370ba5ab
Marcus Koch <kochmarcus00@gmail.com>
2026-06-01 23:02:13 +0200

    added blog posts to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,6 +11,36 @@ export default function sitemap() {
     },
 
     {
+      url: "https://www.ich-habs-im-griff.de/blog/weniger-trinken",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.ich-habs-im-griff.de/blog/30-tage-ohne-alkohol",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.ich-habs-im-griff.de/blog/bin-ich-alkoholabhaengig",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.ich-habs-im-griff.de/blog/alkohol-und-beziehung",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.ich-habs-im-griff.de/blog/alkohol-und-schlaf",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.ich-habs-im-griff.de/blog/alkohol-und-angst",
+      lastModified: new Date(),
+    },
+
+    {
       url: "https://www.ich-habs-im-griff.de/impressum",
       lastModified: new Date(),
     },
