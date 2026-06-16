56cc370415539e4a02e3de216238808b78ab9b21
Marcus Koch <kochmarcus00@gmail.com>
2026-06-16 09:59:11 +0200

    Neue Blogartikel und Sitemap aktualisiert

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -59,5 +59,66 @@ export default function sitemap() {
   url: "https://www.ich-habs-im-griff.de/blog/rueckfall-alkoholfrei",
   lastModified: new Date(),
 },
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/funktionierender-alkoholiker",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/10-warnzeichen-alkoholabhaengigkeit",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/feierabendbier-jeden-tag",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/alkohol-und-langeweile",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/alkohol-und-depression",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/alkohol-und-einsamkeit",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/warum-ich-dachte-ich-haette-alles-im-griff",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/alkohol-und-scham",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/ich-trinke-nur-am-wochenende",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/kontrolliertes-trinken",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/warum-alkoholprobleme-lange-unsichtbar-bleiben",
+  lastModified: new Date(),
+},
+
+{
+  url: "https://www.ich-habs-im-griff.de/blog/warum-der-gedanke-ans-aufhoeren-angst-macht",
+  lastModified: new Date(),
+},
+
   ];
 }
